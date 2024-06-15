cc53feb4a5c7604d3e22ccd22d047be3619b7870
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-06-15 09:39:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,33 @@
+//assignment 23 
+//conditional statement print 10 series of conditional statment describing it and give 5 true result 5 false
+let car = `subaru`
+//1 condition
+console.log(`is car ==subaru? i predict true`)
+console.log(car== `subaru`)
+//2 condition
+console.log(`is car !== subaru? i predict false`)
+console.log(car!== `subaru`)
+//3 condition
+console.log(`is car == toyota? i predict false`)
+console.log(car==`toyota`)
+//4 condition
+console.log(`is car!==toyota? i predict true`)
+console.log(car!==`toyota`)
+//5 condition
+console.log(`is car ==suzuki? i predict false`)
+console.log(car ==`suzuki`)
+ //6 condition
+ console.log(`is car !==suzuki? i predict true`)
+ console.log(car!==`suzuki`)
+ //7 condition
+console.log(`car.length >0? i predict true`)
+console.log(car.length>0)
+//8 condition
+console.log(`car.length<5? i predict false`)
+console.log(car.length<5)
+//9 condition
+console.log(`car == automatic? i predict false`)
+console.log(car== `automatic`)
+//10 condition
+console.log(`car!== automatic ? i predict true`)
+console.log(car !== `automatic`)
